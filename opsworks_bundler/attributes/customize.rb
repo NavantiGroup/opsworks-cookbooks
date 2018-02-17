acd1671c8bdca3aafef064ceb6a720dfec581108
Kiere El-Shafie <kiere@mac.com>
2018-02-16 21:01:43 -0500

    Change bundler executable path
    In an attempt to avoid conflict with vendored version installed by Rubygems

--- opsworks_bundler/attributes/customize.rb
+++ opsworks_bundler/attributes/customize.rb
@@ -9,3 +9,9 @@
 # The following shows how to override the bundler version:
 #
 #normal[:opsworks_bundler][:version] = '1.3.5'
+
+# The vendored version of Bundler installed by RubyGems is conflicting with the one
+# installed by this package.  Attempting to place this version in an alternate location
+# to see if the gems are bundled with this version or the rubygems version.
+# Change made by Kiere El-Shafie on 2018-02-16
+default[:opsworks_bundler][:executable] = '/usr/local/bin/openseas/bundle'
